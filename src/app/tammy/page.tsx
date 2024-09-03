3537f2bbcd05d2fc0afed4fbaf43ac3a651e96a6
tylerdipolito <tyler@jaksapparel.com>
2024-09-03 15:02:02 -0400

    Update page.tsx

--- src/app/tammy/page.tsx
+++ src/app/tammy/page.tsx
@@ -152,16 +152,16 @@ export default function Page() {
           </div>
         </div>
       </section>
-      <section id="hackathons">
+      <section id="experience">
         <div className="space-y-12 w-full py-12">
           <BlurFade delay={BLUR_FADE_DELAY * 13}>
             <div className="flex flex-col items-center justify-center space-y-4 text-center">
               <div className="space-y-2">
                 <div className="inline-block rounded-lg bg-foreground text-background px-3 py-1 text-sm">
-                  Hackathons
+                  Experience
                 </div>
                 <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">
-                  I like building things
+                  I like scaling businesses
                 </h2>
                 <p className="text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
                   During my time in university, I attended{" "}
